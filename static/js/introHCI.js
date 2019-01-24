221169f6d9c58ed8d235a66394cff539da3c4220
Jaclyn Chin <jac289@ucsd.edu>
2019-01-24 10:18:43 -0800

    Commit message

--- static/js/introHCI.js
+++ static/js/introHCI.js
@@ -3,7 +3,7 @@
 // Call this function when the page loads (the "ready" event)
 $(document).ready(function() {
 	initializePage();
-})
+});
 
 /*
  * Function that is called when the document is ready.
@@ -11,8 +11,21 @@ $(document).ready(function() {
 function initializePage() {
 	$("#testjs").click(function(e) {
 		$('.jumbotron h1').text("Javascript is connected");
+		$(".jumbotron p").toggleClass("active");
 	});
+		$("a.thumbnail").click(projectClick);
+	function projectClick(e) {
+		e.preventDefault();
+		$(this).css("background-color", "#000000");
+	     var containingProject = $(this).closest(".project");
+    var description = $(containingProject).find(".project-description");
+    if (description.length == 0) {
+       $(containingProject).append("<div class='project-description'><p>Description of the project.</p></div>");
+    } else {
+       $(containingProject).fadeIn("<div class='project-description'><p>Description of the project.</p></div>");
+	}
 
 	// Add any additional listeners here
 	// example: $("#div-id").click(functionToCall);
+}
 }
